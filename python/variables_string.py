9cf2b1e4809b7c8ab1473a465faf2fa92dce62c1
vthmassians <vahikt@gmail.com>
2016-06-07 23:40:55 -0700

    fixed typo in example

--- python/variables_string.py
+++ python/variables_string.py
@@ -9,7 +9,7 @@ var_string_2 = 'Bob!'
 #
 # concatenate and store the values of two variables in 
 # a third variable using 
-# string concatenation operator '.'
+# string concatenation operator '+'
 #
 var_string_3 = var_string_1 + var_string_2
 

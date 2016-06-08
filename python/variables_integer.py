9197a1fc069d6f98337a031f7a916d19e1187b09
vthmassians <vahikt@gmail.com>
2016-06-07 23:39:47 -0700

    added python examples

---
+++ python/variables_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/py
+
+#
+# store an integer in a variable (whole number)
+#
+var_int_1 = 12
+var_int_2 = 12
+
+#
+# store a floating point in a variable
+#
+var_float = 12.5
+
+#
+# Do some math
+#
+answer_1 = var_int_1 + var_int_2
+answer_2 = var_int_1 + var_int_2 + var_float
+
+print answer_1 # print 24
+print answer_2 # print 36.5
---
+++ python/variables_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#
+# store a string variable
+#
+var_string_1 = 'My name is: '
+var_string_2 = 'Bob!'
+
+#
+# concatenate and store the values of two variables in 
+# a third variable using 
+# string concatenation operator '.'
+#
+var_string_3 = var_string_1 + var_string_2
+
+#
+# print the values
+#
+print var_string_1 # My name is :
+print var_string_2 # Bob!
+print var_string_3 # My name is : Bob!

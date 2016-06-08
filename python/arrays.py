8e338197fc22bd990522e43bcc33a4b0c122ffd0
vthmassians <vahikt@gmail.com>
2016-06-08 07:30:40 -0700

    Added list/array example for python

---
+++ python/arrays.py
@@ -0,0 +1,25 @@
+
+#
+# In python, arrays are called lists. There
+# are other data structures as well that we'll
+# get into later
+#
+# 0 = bob
+# 1 = heather
+# 2 = john
+# 3 = jen
+#
+# 
+list_of_names = ['bob','heather','john','jen'];
+
+list_of_names[0] = 'bob';
+list_of_names[1] = 'heather';
+list_of_names[2] = 'john';
+list_of_names[3] = 'jen';
+
+#
+# print the first element
+#
+print list_of_names[0] # bob
+print list_of_names[3] # jen 
+

312adbc6fdffa4361ecf2e546f2937b876825b57
vthmassians <vahikt@gmail.com>
2016-06-09 17:41:41 -0700

    ruby string examples

---
+++ ruby/variables_string.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+#
+#store a string a variable 
+#
+var_string_1 = 'My name is : '
+var_string_2 = 'Bob!'
+
+#
+# concatenate and store the values of two variables in 
+# a third variable using 
+# string concatenation operator '+'
+#
+var_string_3 = var_string_1 + var_string_2
+
+#
+# print the values
+#
+puts var_string_1
+puts var_string_2
+puts var_string_3

1f164b4dedd0f0a227d02001beafbd8bc229823d
Anish Sujanani <ansujanani@hotmail.com>
2016-06-20 11:00:55 +0530

    added functions and documentation

--- javascript/functions.js
+++ javascript/functions.js
@@ -4,21 +4,55 @@
  * meat in the body
  *
  */
-function showWord()
-{
+function showWord() {
     console.log('Hello World');
 }
+
 
 //Call the function
 showWord(); // Hello World
 
+
 /*
  * Now lets modify the function, and pass an argument
  */
-function showWordAgain( word_to_print )
-{
+function showWordAgain( word_to_print ) {
     console.log( word_to_print ); //print the argument
 }
 
+
 //Call the function, with an argument
 showWordAgain('Hello Again'); // prints "Hello Again"
+
+
+// inner / nested functions
+function outer_function() {          //parent of inner_function ()
+    var test = 10;                   // outer_function()'s variable ie. parent function variable and parameters
+
+    function inner_function() {      // nested / inner functions have access to the parent function's variables
+        test = 30;                   // modifying parent function's variable
+    }
+
+    console.log(test);                // prints outer_function() test value -> 10
+    inner_function();                 // call to inner_function()
+    console.log(test);                // prints outer_function() test value -> 30 (modified by inner function)
+}
+outer_function();    //call to outer_function()
+
+
+
+// inner / nested functions with parameter
+function outer_function(param_var) {          //parent of inner_function ()
+    var test = param_var;                   // outer_function()'s variable = parameter 
+
+    function inner_function() {      // nested / inner functions have access to the parent function's variables and parameters
+        test = 30;                               // modifying parent function's variable
+        var test_2 = param_var;                  // using parent function's parameter
+        console.log(test_2);                     // prints parameter to parent function from within child function
+    }
+
+    console.log(test);                // prints outer_function() test value -> 40
+    inner_function();                 // call to inner_function()
+    console.log(test);                // prints outer_function() test value -> 30 (modified by inner function)
+}
+outer_function('40');    //call to outer_function() with 40 as parameter
--- javascript/strings.js
+++ javascript/strings.js
@@ -6,6 +6,7 @@
 var var_string_1 = 'My name is : ';
 var var_string_2 = 'Bob!';
 
+
 /*
  * concatenate and store the values of two variables in 
  * a third variable using 
@@ -13,9 +14,63 @@ var var_string_2 = 'Bob!';
  */
 var var_string_3 = var_string_1 + var_string_2;
 
+
 /*
  * print the values
  */
 console.log(var_string_1); // My name is :
 console.log(var_string_2); // Bob!
 console.log(var_string_3); // My name is : Bob!
+
+
+/*
+ * String functions
+ *  Strings are immutable, therefore my_str.someFunction() will not change the original string
+ *  Modified strings have to be assigned to some variable. my_str = my_str.someFunction() will work.
+*/
+var my_str = 'test string';
+
+
+// length
+my_str.length;
+
+
+// access a character by its position in the string
+my_str[0]  // first character
+my_str[200]   // will return 'undefined' if character at index 200 (ie. character #201) does not exist
+   //OR
+my_str.charAt(0);
+my_str.charAt(200);   // will return '' (no character) better; avoid undefined errors
+
+
+// convert to upper case
+my_str = my_str.toUpperCase();  
+   // capitalize a letter
+my_str = my_str.charAt(0).toUpperCase();
+
+
+//convert to lower case
+my_str.toLowerCase();
+
+
+// slicing a string (forwards)
+var new_str = my_str.slice(4); // slice( (number) ) the string will have 'number' characters removed from the start
+// ie. first four characters removed
+
+
+//slicing a string (backwards)
+new_str = my_str.slice(-4) // slice( -number ) will have 'number' characters removed from the end of string
+// ie. last 4 characters removed
+
+
+// Capitalize the first letter of a string
+my_str = my_str.charAt(0).toUpperCase() + my_str.slice(1);
+// will capitalize the first letter of the string
+// explaination: 
+// Convert the character at the first letter of the string (index : 0) to uppercase
+// and append it to the string sliced at position 1 (ie. the first character removed)
+// = Capitalized first letter + (original string (minus) the first letter)
+
+
+// capitalizations on other letters: 
+// slice the stirng backwards, capitalize the letter, append the first portion, the capital letter, and the rest of string
